dde43fee91b2d7eb283825f0a9b8ba67761400ae
David R. Piegdon <dgit@piegdon.de>
2021-07-22 19:52:56 +0000

    demonstrate recursionlevel 1

---
+++ Makefile
@@ -0,0 +1,18 @@
+
+.PHONY: all clean test
+
+all: jumptable
+
+clean:
+	-rm jumptable
+
+test: jumptable
+	./jumptable 0 0
+	./jumptable 1 0
+	./jumptable 2 0
+	./jumptable 3 0
+	./jumptable 3 1
+	./jumptable 3 2
+	./jumptable 3 3
+
+jumptable: jumptable.c
--- jumptable.c
+++ jumptable.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 #define _ESCAPE(...) __VA_ARGS__
 
 #define JUMPTABLE(RETTYPE, NAME, ARGS, BADVALUE, ARRAY, INDEXTYPE, INDEXNAME, INDEXVALUE, INDEXBITS, ...)	\
-	RETTYPE NAME(ARGS){											\
-		const RETTYPE (*jTable[1 << INDEXBITS])(ARGS) = {ARRAY};					\
+	RETTYPE NAME(ARGS) {											\
+		const RETTYPE (*jTable[1 << INDEXBITS]) (ARGS) = {ARRAY};					\
 		INDEXTYPE INDEXNAME = (INDEXVALUE) & ((1 << INDEXBITS)-1);					\
-		RETTYPE (*fun)(ARGS) = jTable[INDEXNAME];							\
+		RETTYPE (*fun) (ARGS) = jTable[INDEXNAME];							\
 		if (NULL == fun)										\
 			return BADVALUE;									\
 		else												\
@@ -15,45 +16,77 @@
 
 #define MYJUMPTABLE(NAME, INDEXTYPE, INDEXNAME, INDEXVALUE, INDEXBITS, ARRAY)					\
 	JUMPTABLE(int,												\
-		NAME,												\
-		_ESCAPE(unsigned value, int foo),								\
+		_ESCAPE(NAME),											\
+		_ESCAPE(int foo, int bar),									\
 		-1,												\
 		_ESCAPE(ARRAY),											\
-		INDEXTYPE,											\
-		INDEXNAME,											\
-		INDEXVALUE,											\
-		INDEXBITS,											\
-		value,												\
-		foo)
+		_ESCAPE(INDEXTYPE),										\
+		_ESCAPE(INDEXNAME),										\
+		_ESCAPE(INDEXVALUE),										\
+		_ESCAPE(INDEXBITS),										\
+		foo,												\
+		bar)
 
-
-int d0(unsigned value, int foo)
+int d0(int foo, int bar)
 {
-	printf("A %u %d\n", value, foo);
+	printf("A %d %d\n", foo, bar);
 	return 1;
 }
 
-int d1(unsigned value, int foo)
+int d1(int foo, int bar)
 {
-	printf("B %u %d\n", value, foo);
+	printf("B %d %d\n", foo, bar);
 	return 1;
 }
 
-int d2(unsigned value, int foo)
+int d2(int foo, int bar)
 {
-	printf("C %u %d\n", value, foo);
+	printf("C %d %d\n", foo, bar);
 	return 1;
 }
 
-int d3(unsigned value, int foo)
+int d3(int foo, int bar)
 {
-	printf("D %u %d\n", value, foo);
+	printf("D %d %d\n", foo, bar);
 	return 1;
 }
 
-MYJUMPTABLE(decode, unsigned, p0, value & 0x3, 3, _ESCAPE(d0, d1, d2, d3));
+int _y0(int foo, int bar)
+{
+	printf("W %d %d\n", foo, bar);
+	return 1;
+}
+
+int _y1(int foo, int bar)
+{
+	printf("X %d %d\n", foo, bar);
+	return 1;
+}
+
+int _y2(int foo, int bar)
+{
+	printf("Y %d %d\n", foo, bar);
+	return 1;
+}
+
+int _y3(int foo, int bar)
+{
+	printf("Z %d %d\n", foo, bar);
+	return 1;
+}
+
+MYJUMPTABLE(decode2, unsigned, p0, bar, 2, _ESCAPE(_y0, _y1, _y2, NULL));
+MYJUMPTABLE(decode1, unsigned, p0, foo, 2, _ESCAPE(d0, d1, d2, decode2));
 
 int main(int argc, char**argv)
 {
-	printf("ret %d\n", decode(argc, argc & 3));
+	if(argc == 3) {
+		int a = atoi(argv[1]);
+		int b = atoi(argv[2]);
+		int r = decode1(a, b);
+		if(r < 0)
+			printf("ret %d for %d %d\n\n", r, a, b);
+	} else {
+		printf("2 args plz.\n");
+	}
 }
